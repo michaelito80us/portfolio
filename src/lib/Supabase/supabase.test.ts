067dcfddc6d6ad84e5035722040dcf8b0256993c
Michael Epelboim <mepelboim@outlook.com>
2025-02-25 13:46:32 +0100

    fix: supabase tests use import intead of require

--- src/lib/Supabase/supabase.test.ts
+++ src/lib/Supabase/supabase.test.ts
@@ -1,5 +1,3 @@
-import { supabase } from './supabase';
-
 describe('Supabase Client', () => {
   const originalEnv = process.env;
 
@@ -12,32 +10,26 @@ describe('Supabase Client', () => {
     process.env = originalEnv;
   });
 
-  it('should be configured with environment variables', () => {
+  it('should be configured with environment variables', async () => {
+    const { supabase } = await import('./supabase');
     expect(supabase).toBeDefined();
     expect(supabase.from('user_preferences')).toBeDefined();
     expect(supabase.from('analytics_events')).toBeDefined();
   });
 
   it('should have auth disabled', async () => {
+    const { supabase } = await import('./supabase');
     const { data } = await supabase.auth.getSession();
     expect(data.session).toBeNull();
   });
 
-  it('should throw error if SUPABASE_URL is missing', () => {
+  it('should throw error if SUPABASE_URL is missing', async () => {
     delete process.env.NEXT_PUBLIC_SUPABASE_URL;
-    expect(() => {
-      jest.isolateModules(() => {
-        require('./supabase');
-      });
-    }).toThrow('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+    await expect(import('./supabase')).rejects.toThrow('Missing env.NEXT_PUBLIC_SUPABASE_URL');
   });
 
-  it('should throw error if SUPABASE_ANON_KEY is missing', () => {
+  it('should throw error if SUPABASE_ANON_KEY is missing', async () => {
     delete process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-    expect(() => {
-      jest.isolateModules(() => {
-        require('./supabase');
-      });
-    }).toThrow('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
+    await expect(import('./supabase')).rejects.toThrow('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
   });
 });
